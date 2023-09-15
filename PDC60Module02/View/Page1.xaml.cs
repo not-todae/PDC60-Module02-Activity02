0a543c0b276b5761afd0b7fec50b256fa31a7684
not-todae <echon.davidaaron@auf.edu.ph>
2023-09-15 11:33:25 +0800

    first commit

---
+++ PDC60Module02/View/Page1.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PDC60Module02.ViewModel;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PDC60Module02.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+            BindingContext = new StudentViewModel();
+        }
+    }
+}
