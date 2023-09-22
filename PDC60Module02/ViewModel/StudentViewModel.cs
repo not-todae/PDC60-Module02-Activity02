ffbbb9d006e5b354ebf55b3dfbb1bf2d889f0061
not-todae <echon.davidaaron@auf.edu.ph>
2023-09-22 11:07:54 +0800

    validation-behavior

--- PDC60Module02/ViewModel/StudentViewModel.cs
+++ PDC60Module02/ViewModel/StudentViewModel.cs
@@ -12,6 +12,8 @@ namespace PDC60Module02.ViewModel
         public student StudentSet { get; set; }
         public Command SubmitEntry { get; set; }
 
+        public Command ClearEntry { get; set; }
+
         public string SubjectCodeEntry { get; set; }
         public string SubjectCodeDisplay { get; set; }
 
@@ -44,6 +46,26 @@ namespace PDC60Module02.ViewModel
             OnPropertyChanged(nameof(SubjectUnitEntry));
             }
                 );
+
+            ClearEntry = new Command(() =>
+            {
+                SubjectCodeDisplay = "";
+                SubjectCodeEntry = "";
+                SubjectTitleDisplay = "";
+                SubjectTitleEntry = "";
+                SubjectUnitDisplay = "";
+                SubjectUnitEntry = "";
+
+                OnPropertyChanged(nameof(SubjectCodeDisplay));
+                OnPropertyChanged(nameof(SubjectCodeEntry));
+
+                OnPropertyChanged(nameof(SubjectTitleDisplay));
+                OnPropertyChanged(nameof(SubjectTitleEntry));
+
+                OnPropertyChanged(nameof(SubjectUnitDisplay));
+                OnPropertyChanged(nameof(SubjectUnitEntry));
+            }
+    );
         }
         public event PropertyChangedEventHandler PropertyChanged;
         public void OnPropertyChanged(string propertyName)
